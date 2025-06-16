d53baafed8f001fae7323445847940f0b7039bab
Harshavardhan-1212 <hsingireddy4990@conestogac.on.ca>
2025-06-15 21:22:58 -0400

    Feat: Validate input for  calculateCanvasSize function

--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -2,5 +2,17 @@ export default function calculateCanvasSize(
   length: string,
   width: string
 ) {
+
+  const parsedLength = Number(length);
+  const parsedWidth = Number(width);
+
+  if (isNaN(parsedLength) || isNaN(parsedWidth)) {
+    throw new Error("Invalid input. length and width must be number.");
+  }
+
+  if (parsedLength <= 0 || parsedWidth <= 0) {
+    throw new Error("Length and width must be greater than zero");
+  }
+
   return parseInt(length) * parseInt(width);
 }
