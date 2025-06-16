ddd1c21c09a8f0c4bd334f2d6422208a5446bd7b
Harshavardhan-1212 <hsingireddy4990@conestogac.on.ca>
2025-06-15 21:36:18 -0400

    Feat: Validate input for paintRequiredCalculator function

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -1,3 +1,14 @@
 export default function paintRequiredCalculator(area: number, coveragePerLiter: number): number {
-  return area / coveragePerLiter;
+
+  if (area < 0) {
+    throw new Error("Area must be non negative.");
+  }
+
+  if (coveragePerLiter <= 0) {
+    throw new Error("Coverage per liter must be greater than zero");
+  }
+
+  const paintRequired = area / coveragePerLiter;
+
+  return parseFloat(paintRequired.toFixed(2));
 }
